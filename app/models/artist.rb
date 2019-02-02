80b70e7c454f89d7f09d38fb490831794b3a05a6
stubrew24 <sewelly@gmail.com>
2019-02-02 22:37:42 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,44 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, gallery, self)
+  end
+
+  def paintings
+    Painting.all.select {|x| x.artist == self}
+  end
+
+  def galleries
+    paintings.map {|x| x.gallery}
+  end
+
+  def cities
+    galleries.map {|x| x.city}
+  end
+
+  def paintings_per_year
+    paintings.count.to_f / @years_experience.to_f
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_experience
+    all.sum {|x| x.years_experience}
+  end
+
+  def self.most_prolific
+    @@all.max_by {|x| x.paintings_per_year}
   end
 
 end
--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,12 +1,23 @@
 class Painting
 
-  attr_reader :title, :price
+  attr_reader :title, :price, :gallery,:artist
 
   @@all = []
 
-  def initialize(title, price)
+  def initialize(title, price, gallery, artist)
     @title = title
     @price = price
+    @artist = artist
+    @gallery = gallery
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.total_price
+    all.sum {|x| x.price}
   end
 
 end
