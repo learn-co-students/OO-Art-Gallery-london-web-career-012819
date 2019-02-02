4851f8d2b31bb825f15d0763b72ca1ec7b817c23
SofiaFredriksson <sofiapkf@gmail.com>
2019-02-02 12:09:59 +0000

    Done apart from prolific

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,44 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self }
+  end
+
+  def galleries
+    paintings.map { |painting| painting.gallery  }
+  end
+
+  def cities
+    galleries.map { |gallery| gallery.city  }
+  end
+
+  def self.total_experience
+    arr = []
+    arr = @@all.map { |artist| artist.years_experience  }
+    arr.reduce(:+)
+  end
+
+  # def self.most_prolific
+  # arr = @@all.sort_by { |artist| artist.years_experience }
+  # sorted_arr = arr.reverse
+  # artist_arr = @@all.sort_by { |artist| artist.name }
+  # end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,21 @@
 require_relative '../config/environment.rb'
 
 
+peder = Artist.new("Peder", 5)
+fia = Artist.new("Fia", 10)
+luc = Artist.new("Luc", 15)
+erika = Artist.new("Erika", 3)
+
+tate = Gallery.new("Tate", "London")
+moma = Gallery.new("Museum of Modern Art", "New York")
+olympic = Gallery.new("Olympic", "Lausanne")
+v_and_a = Gallery.new("Victoria & Albert", "London")
+
+p1 = Painting.new("Flower", 80, luc, moma)
+p2 = Painting.new("Water", 90, fia, tate)
+p3 = Painting.new("Sun", 10, erika, olympic)
+p4 = Painting.new("Rage", 120, peder, v_and_a)
+p5 = Painting.new("Sisters", 200, fia, moma)
 
 
 binding.pry
