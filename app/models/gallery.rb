6c9725fb5c7c7ee5baf8955c204c841b678450ba
github username <github email address>
2019-02-03 20:42:59 +0000

    Done.

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -2,9 +2,41 @@ class Gallery
 
   attr_reader :name, :city
 
+  @@all = []
+
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    # * `Gallery#paintings`
+    #   * Returns an `array` of all paintings in a gallery
+    Painting.all.select { |painting| painting.gallery == self }
+  end
+
+  def artists
+    # * `Gallery#artists`
+    #   * Returns an `array` of all artists that have a painting in a gallery
+    paintings.map { |painting| painting.artist }
+  end
+
+  def artist_names
+    # * `Gallery#artist_names`
+    #   * Returns an `array` of the names of all artists that have a painting in a gallery
+    self.artists.map { |artist| artist.name }
+  end
+
+  def most_expensive
+    # * `Gallery#most_expensive_painting`
+    #   * Returns an `instance` of the most expensive painting in a gallery
+     arr = self.paintings.sort_by {|painting| painting.price}.reverse
+     arr[0]
   end
 
 end
