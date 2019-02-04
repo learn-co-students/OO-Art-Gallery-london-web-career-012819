892f800fdb424c92c2bf95511aac01906f2a5967
Kian Edalat <akedalat@gmail.com>
2019-02-04 21:32:05 +0000

    OO art gallery completed by Kian

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,46 @@
 class Artist
 
-  attr_reader :name, :years_experience
+attr_accessor :name, :years_experience
+@@all = []
 
-  def initialize(name, years_experience)
-    @name = name
-    @years_experience = years_experience
+def initialize(name, years_experience)
+  @name = name
+  @years_experience = years_experience
+  @@all << self
+end
+
+def self.all
+@@all
+end
+
+def paintings
+  Painting.all.select do |painting|
+    painting.artist == self
+    end
+end
+
+def galleries
+    self.paintings.map do |painting|
+      painting.gallery
+    end
+end
+
+def cities
+  self.galleries.map do |gallery|
+    gallery.city
   end
+end
+
+def self.total_experience
+total_experience = 0
+  @@all.each do |artist|
+    total_experience += artist.years_experience
+  end
+  total_experience
+end
+
+def create_painting(title, price, gallery)
+Painting.new(title, price, self, gallery)
+end
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,10 +1,38 @@
 class Gallery
 
-  attr_reader :name, :city
-
+  attr_accessor :name, :city
+  @@all =[]
   def initialize(name, city)
     @name = name
     @city = city
+    @@all << self
   end
+def self.all
+@@all
+end
+
+def paintings
+  Painting.all.select do |painting|
+    painting.gallery == self
+  end
+end
+
+def artists
+  self.paintings.map do |painting|
+    painting.artist
+  end
+end
+
+def artist_names
+  self.artists.map do |artist|
+    artist.name
+  end
+end
+
+def self.most_expensive_painting
+  Painting.all.max_by do |painting|
+    painting.price
+  end
+end
 
 end
